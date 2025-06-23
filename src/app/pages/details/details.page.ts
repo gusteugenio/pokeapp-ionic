acb514bb0bf6be4d6c6808197b4fd08203624a67
gusteugenio <gustavotertuliano@chuva-inc.com>
2025-06-23 09:20:27 -0300

    feat: Adiciona gráfico com mais informações de pokemons

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokeService } from 'src/app/services/poke.service';
+import { PokemonSpecies } from 'src/app/models/pokemon-species.model';
+import { ChartConfiguration } from 'chart.js';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Component({
   selector: 'app-details',
@@ -12,6 +15,23 @@ export class DetailsPage implements OnInit {
   pokemon: any = null;
   description: string = '';
 
+  public barChartLabels: string[] = ['HP', 'Attack', 'Defense', 'Special Attack', 'Special Defense', 'Speed'];
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'],
+    datasets: [
+      {
+        data: [],
+        label: 'Base Stats',
+        backgroundColor: '#ffcb05',
+        borderColor: '#3b4cca',
+        borderWidth: 1,
+        barThickness: 20,
+        categoryPercentage: 0.7,
+        barPercentage: 0.8
+      }
+    ]
+  };
+
   constructor(
     private route: ActivatedRoute,
     private pokeService: PokeService
@@ -20,11 +40,12 @@ export class DetailsPage implements OnInit {
   ngOnInit() {
     const name = this.route.snapshot.paramMap.get('name');
     if (name) {
-      this.pokeService.getPokemonByNameOrId(name).subscribe(data => {
+      this.pokeService.getPokemonByNameOrId(name).subscribe((data: Pokemon) => {
         this.pokemon = data;
+        this.barChartData.datasets[0].data = data.stats.map((s: any) => s.base_stat);
       });
 
-      this.pokeService.getPokemonSpecies(name).subscribe(species => {
+      this.pokeService.getPokemonSpecies(name).subscribe((species: PokemonSpecies) => {
         const entry = species.flavor_text_entries.find(
           (e: any) => e.language.name === 'en'
         );
--- src/app/services/poke.service.ts
+++ src/app/services/poke.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { PokemonSpecies } from '../models/pokemon-species.model'
 
 @Injectable({
   providedIn: 'root'
