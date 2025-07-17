461b89577aee90ebbe1a957c0838c6762d83f3df
gusteugenio <gustavotertuliano@chuva-inc.com>
2025-07-16 23:18:27 -0300

    feat: Adiciona serviço para autenticação

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseUrl = 'http://localhost:4000/auth';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login(credentials: { email: string, password: string }) {
+    return this.http.post<{ token: string, id: string }>(`${this.baseUrl}/login`, credentials);
+  }
+
+  register(data: { username: string, email: string, password: string, gender: string }) {
+    return this.http.post(`${this.baseUrl}/register`, data);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+}
