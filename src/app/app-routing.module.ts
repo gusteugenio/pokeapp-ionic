af06e71ad6c409f2bace1121b7cc915949c80aed
gusteugenio <gustavotertuliano@chuva-inc.com>
2025-07-16 23:19:35 -0300

    chore: Ajusta controle de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -9,20 +10,32 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'details/:name',
-    loadChildren: () => import('./pages/details/details.module').then( m => m.DetailsPageModule)
+    loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'favorites',
-    loadChildren: () => import('./pages/favorites/favorites.module').then( m => m.FavoritesPageModule)
+    loadChildren: () => import('./pages/favorites/favorites.module').then(m => m.FavoritesPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'trainer-area',
-    loadChildren: () => import('./pages/trainer-area/trainer-area.module').then( m => m.TrainerAreaPageModule)
+    loadChildren: () => import('./pages/trainer-area/trainer-area.module').then(m => m.TrainerAreaPageModule),
+    canActivate: [AuthGuard]
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
+  }
 ];
 
 @NgModule({
